7a92ae516512e848291920a78217a388350c744a
carlos <carlos10.ramundo@gmail.com>
2022-02-01 06:56:45 -0300

    Inicio de Proyecto Test para Telecom

---
+++ app/src/main/java/com/example/test/ui/device/DeviceDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.example.test.ui.device
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.test.R
+
+class DeviceDetailFragment : Fragment(R.layout.fragment_device_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/test/ui/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.test.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.test.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
